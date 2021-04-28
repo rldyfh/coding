090d95eaacce292ac859da1c668d26cca88193b5
RY <45328298+rldyfh@users.noreply.github.com>
2021-04-28 20:42:31 +0900

    lv02

---
+++ level_02/hIndex.java
@@ -0,0 +1,21 @@
+package level_02;
+
+import java.util.*;
+
+public class hIndex {
+
+	public int solution(int[] citations) {
+        int answer = 0;
+        
+        Arrays.sort(citations);
+        for(int i = 0 ; i < citations.length ; i++){
+            int h = citations.length - i;
+            if(citations[i]  >= h){
+                answer = h;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
