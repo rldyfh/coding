6047b68e08686846e47bf6357b09e2e490e0455f
RY <45328298+rldyfh@users.noreply.github.com>
2021-05-07 21:46:12 +0900

    lv03

--- level_03/network.java
+++ level_03/network.java
@@ -20,7 +20,7 @@ public class network {
 		
 		for(int j = 0  ; j < computers.length ; j++) {
 			if( i !=j && computers[i][j] == 1 && check[j] == false) {
-				dfs(computers, j , check);
+				check = dfs(computers, j , check);
 			}
 		}
 		return check;
