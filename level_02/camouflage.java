d0457155ce3027a7b3799eb61edcd9f90b579c98
RY <45328298+rldyfh@users.noreply.github.com>
2021-05-10 17:05:08 +0900

    lv02

---
+++ level_02/camouflage.java
@@ -0,0 +1,21 @@
+package level_02;
+
+import java.util.*;
+
+public class camouflage {
+	public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0 ; i < clothes.length ; i++){
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        for(String key : map.keySet()){
+            answer *= (map.get(key)+1);
+        }
+        
+    
+        
+        return answer - 1;
+    }
+}
