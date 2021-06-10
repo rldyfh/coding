13f405aef06cfe893dc2344fd403b3d2a4b4e4b7
RY <45328298+rldyfh@users.noreply.github.com>
2021-06-10 18:05:16 +0900

    lv02

---
+++ level_02/eatGround.java
@@ -0,0 +1,25 @@
+package level_02;
+
+import java.util.*;
+
+public class eatGround {
+	
+	int solution(int[][] land) {
+        int answer = 0;
+        
+        for(int i=1; i<land.length; i++) {
+			land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+			land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+			land[i][2] += Math.max(land[i-1][1], Math.max(land[i-1][0], land[i-1][3]));
+			land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][2]));
+		}
+        Arrays.sort(land[land.length-1]);
+        answer = land[land.length-1][3];
+        
+        
+
+        return answer;
+    }
+
+
+}
